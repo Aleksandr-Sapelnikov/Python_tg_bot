49a712d2e148022720fe84229be149a2d9cf95d2
Aleksandr-Sapelnikov <141849862+Aleksandr-Sapelnikov@users.noreply.github.com>
2023-08-09 18:56:20 +0300

    Update config.py

--- project/at_proj/config.py
+++ project/at_proj/config.py
@@ -1,5 +1,5 @@
-rapid_api_key = "962b98a548msh1dd81b569a78ec7p1538eejsn873316f46a92"
-bot_token = '5758636146:AAG6tkWwx1to2aNPEAZQVDIav1W-mGhJyWA'
+rapid_api_key = "your api key"
+bot_token = 'your bot token'
 url_for_city_id = "https://hotels4.p.rapidapi.com/locations/v3/search"
 url_for_hotel_list = "https://hotels4.p.rapidapi.com/properties/v2/list"
 url_for_photo = "https://hotels4.p.rapidapi.com/properties/v2/get-summary"
